96bce5dff136e205f7f9311786b38b09d745d89d
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2023-05-02 14:45:27 +0600

    json data created

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const chefInfo = require('./data/chefInfo.json');
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Master chef!");
 });
 
+app.get("/chefInfo", (req, res) => {
+    res.send(chefInfo);
+});
+
+app.get("/chefInfo/:id", (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const chefInformation = chefInfo.filter(chef => chef._id === id);
+    console.log(chefInformation);
+    res.send(chefInformation);
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
-})
+});
