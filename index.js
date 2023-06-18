1f4d370d42f9156a1811c952b711585f1dde26c5
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2023-06-18 10:31:42 +0600

    clone commit chef-server

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 const chefInfo = require('./data/chefInfo.json');
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Master chef!");
